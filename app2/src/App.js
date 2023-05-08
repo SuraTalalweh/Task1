b750461c5af1e8a9426550225ce26e509c0de868
SuraTalalweh <132947227+SuraTalalweh@users.noreply.github.com>
2023-05-08 23:39:27 +0300

    Add files via upload

---
+++ app2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About/About.jsx';
+import Home from './components/About.jsx';
+
+function App() {
+  return (
+    <div>
+      <About/>
+      <Home/>
+    </div>
+
+  );
+}
+
+export default App;
